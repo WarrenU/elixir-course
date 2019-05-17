74d36c4f270163557f076a3e406321a1a6170468
Warren Urbina <warren.urbina@gmail.com>
2019-05-17 07:31:32 -0700

    demonstrating value of unpacking 2 iterables in a list comprehension

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,7 +3,22 @@ defmodule Cards do
   Functions that perform on a deck of cards.
   """
   def create_deck do
-    ["2", "3", "4", "5", "6", "7", "8", "9", "K", "Q", "A"]
+    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    suits = ["Spade", "Club", "Diamond", "Heart"]
+
+    # Worse approach (solution 1):
+    # cards = for val <- values do
+    #   for suit <- suits do
+    #     "#{val} of #{suit}"
+    #   end
+    # end
+    # List.flatten(cards)
+
+    # Better approach, (solution 2):
+    for suit <- suits, val <- values do
+      "#{val} of #{suit}"
+    end
+    
   end
 
   def shuffle(deck) do
