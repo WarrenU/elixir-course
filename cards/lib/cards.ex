0fed578d70c0daf4e934da9203ec5e5169e00087
Warren Urbina <warren.urbina@gmail.com>
2019-05-17 07:11:18 -0700

    implement contains function

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,16 +1,6 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for Cards.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
+  Functions that perform on a deck of cards.
   """
   def create_deck do
     ["2", "3", "4", "5", "6", "7", "8", "9", "K", "Q", "A"]
@@ -18,5 +8,9 @@ defmodule Cards do
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
