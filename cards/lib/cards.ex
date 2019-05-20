57fc89afbd81b4df7de5349ef50adba25c34a158
Warren Urbina <warren.urbina@gmail.com>
2019-05-19 19:30:09 -0700

    create_hand function, using pipe op

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -11,6 +11,12 @@ defmodule Cards do
     end    
   end
 
+  def create_hand(hand_size) do
+    Cards.create_deck 
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
