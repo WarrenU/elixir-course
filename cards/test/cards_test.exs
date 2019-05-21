da13f523df1792b8865fc96ca07f9ba36d05cbd5
Warren Urbina <warren.urbina@gmail.com>
2019-05-21 08:27:54 -0700

    add shuffle test

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -6,4 +6,12 @@ defmodule CardsTest do
     deck_length = length(Cards.create_deck)
     assert deck_length == 52
   end
+
+  @doc """
+    This test *may* fail, in the case that shuffle returns the exact same ordering.
+  """
+  test "shuffle a deck randomizes it" do
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
+  end
 end
